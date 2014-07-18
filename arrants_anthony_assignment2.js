e0ff9f570fbc40abc5f33b42ec07b0720b2bdd24
Anthony Arrants <antnya@fullsail.edu>
2014-07-18 19:54:11 -0400

    This is the last of the last.

--- arrants_anthony_assignment2.js
+++ arrants_anthony_assignment2.js
@@ -1,59 +1,86 @@
-// alert("JavaScript works!");
+// Prompt
 
-//Anthony Arrants
-//SDI 1407
-//Project 2
-//July 17, 2014
+var thePrompt = prompt("What is your name?");
 
-// variables start here
-var greeting = "What is your name?"; 
-function getUserName(){
-return prompt(greeting);
-}
-function yourFunction(){
- alert("Hello "+getUserName());
-}
-yourFunction();
+// Procedure
 
-
-
-
-
-
-
-// procedure
-
-var nameFunction = function (n1, n2) {
+var nameFunction = function(personName) {
     
-    console.log("Hey there " + n1 + " and " + n2 + ", glad to see you here today! Let's go on a road trip!");
-
-}
-
-
-
-// Confrimation
-
-var readyToGo = confirm("Are you ready to go?!");
-
-if (readyToGo == true) {
+    if (personName === "Lee") {
+        
+        console.log("Glad to see you here. You're familiar face.")
+        
+    } else {
     
-    console.log("Hope you're ready for a long trip!")
-    
-} else {
-    
-    console.log("Get ready! We need to hit the road!");
+        console.log("Hello,", personName,"I don't know you, but you're welcome to come along.")
+        
+    }
+
 };
 
 
+// Confirm and Boolean
 
-//
+var theConfirmation = confirm("Are you ready to go?");
 
-//
+var confirmFunc = function(confirmation, personName) {
 
-//
+    var counter = 0;
+    var endCount = 4;
+    
+    while (counter < endCount) {
+        
+        if (!confirmation) {
+            
+            console.log("I only have", endCount - counter, "minutes left!");
+            
+            counter += 1;
+            
+        } else {
+            
+            console.log("Let's go.");
+            
+            break;
+            
+        }
+        
+    }
+    
+    return confirmation;    
+};
 
-//
+// Number function
 
-// function calls
+var numberFunction = function(number) {
+    
+    var i;
+    var endCounter = number; 
+    
+    for (i = 0; i < endCounter; i++) {
+        
+        number -= 1;
+        
+        console.log("We have " + number + " minutes until we reach our destination.");
+        
+    }
+    
+    return number;
+}
 
-nameFunction("Lee", "Andrew")
+// String function
+var stringFunction = function(string1, string2) {
+    
+    return string1 + " " + string2;
+    
+}
+
+// Function Calls
+nameFunction(thePrompt);
+var myReturn = confirmFunc(theConfirmation, thePrompt);
+var myNumber = numberFunction(185);
+var myString = stringFunction("Glad we finally made it here", "Where to now?");
+
+// Outputs
+console.log("Ready to go:",myReturn);
+console.log("We need " + myNumber + " gallons of gas to get to our destination.");
+console.log(myString);
